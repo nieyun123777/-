2c64661eed04574367e36911c491172bdc9e213f
nieyun123777 <1627997727@qq.com>
2015-07-05 22:42:59 +0800

    test  分支
    test分支

--- 瞄点/AppDelegate.m
+++ 瞄点/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"hahah");
+    NSLog(@"hahahha");
     // Override point for customization after application launch.
     return YES;
 }
